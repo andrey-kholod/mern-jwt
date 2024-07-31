abc2d72f10798eeca4347ca0194039e2a087dcb2
Andrey Holod <andrejholod77@gmail.com>
2024-07-31 14:25:47 +0400

    Basic router setup

--- client/src/App.tsx
+++
@@ -1,6 +0,0 @@
-const App = () => {
-  return (
-    <h1 className="text-red-700">App</h1>
-  )
-}
-export default App
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Profile from './pages/Profile'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />
+  },
+  {
+    path: '/about',
+    element: <About />
+  },
+  {
+    path: '/sign-in',
+    element: <SignUp />
+  },
+  {
+    path: '/profile',
+    element: <Profile />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
