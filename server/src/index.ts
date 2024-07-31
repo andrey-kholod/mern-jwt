0abaf6da3c03fd66364f2ed2864c908f7bf49c16
Andrey Holod <andrejholod77@gmail.com>
2024-07-31 19:39:46 +0400

    Create User model

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,19 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+
+dotenv.config()
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT || 3000
+
+mongoose.connect(process.env.MONGO_URI!)
+    .then(() => {
+        console.log('Connected to MongoDB')
+    })
+    .catch((err) => {
+        console.log(err)
+    })
 
 app.listen(PORT, () => {
     console.log('Server is listening on port 3000')
